ee9da5694f294b312c81afa5512fbb56b81170e3
huanggang <huanggang@1391.com>
2017-09-14 17:09:16 +0800

    配置文件功能测试

--- microsrv-bizapp/bizapp-userapi/src/main/java/com/hgframework/microsrv/bizapp/userapi/controller/UserController.java
+++ microsrv-bizapp/bizapp-userapi/src/main/java/com/hgframework/microsrv/bizapp/userapi/controller/UserController.java
@@ -2,6 +2,7 @@ package com.hgframework.microsrv.bizapp.userapi.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +16,12 @@ public class UserController {
 
     private Logger logger = LoggerFactory.getLogger(UserController.class);
 
+    @Value("${spring.zipkin.base-url}")
+    private String zikpinUrl;
+
     @RequestMapping(value = "/user/create", method = {RequestMethod.GET})
     public String createUser(@RequestParam(value = "username") String username, @RequestParam(value = "userpwd") String userpwd) {
-        logger.info("request user create");
+        logger.info("request user create" + this.zikpinUrl);
         StringBuffer sb = new StringBuffer().append(username).append(":").append(userpwd);
         return sb.toString();
 
